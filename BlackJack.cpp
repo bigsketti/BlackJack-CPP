eb6cbbd91877e4cead8d439b436aeb6728544550
bigsketti <127629687+bigsketti@users.noreply.github.com>
2024-05-05 19:17:21 -0400

    Update BlackJack.cpp
    Started main game loop

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -1,20 +1,20 @@
-// BlackJack.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "card.hpp"
+#include "player.hpp"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    Player user(1000);
+    Player house(100000);
+
+    std::cout << "Welcome to BlackJack!\n";
+    std::cout << "Time to gamble your life savings away!\n";
+    std::cout << "You have $" << user.GetCash() << " to start.\n";
+
+    std::cout << "Place your bet: ";
+    int bet;
+    std::cin >> bet;
+    user.SetBet(bet);
+
+    std::cout << "You bet $" << user.GetBet() << ".\n";
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
