35a8f4a25aa51bda8b73175c2efddf8b5162db04
bigsketti <127629687+bigsketti@users.noreply.github.com>
2024-05-05 19:23:59 -0400

    Update card.cpp
    tweaks

--- card.cpp
+++ card.cpp
@@ -1,29 +1,35 @@
 #include "card.hpp"
 
-Card::Card() {	// Default constructor
+//default constructor
+Card::Card() {
 	value = 0;
 	suit = ' ';
-	isFaceUp = false;
+	isFaceUp = true;
 }
 
-Card::Card(int value, char suit) {	// Constructor
+//constructor with parameters
+Card::Card(int value, char suit) {
 	this->value = value;
 	this->suit = suit;
 	isFaceUp = false;
 }
 
+//flips the card over
 void Card::ToggleIsFaceUp() {
 	isFaceUp = !isFaceUp;
 }
 
+//returns whether the card is face up or not
 bool Card::GetIsFaceUp() const {
 	return isFaceUp;
 }
 
+//returns the value of the card
 int Card::GetCardValue() const {
 	return value;
 }
 
+//prints the card
 void Card::PrintCard() {
 	if (isFaceUp) {
 		std::cout << value << suit << " ";
