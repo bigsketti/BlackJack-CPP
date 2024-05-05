fb09664d51eca1b76ca44bcc4f118d16d1353fed
bigsketti <127629687+bigsketti@users.noreply.github.com>
2024-05-05 19:16:17 -0400

    Update player.cpp
    just small tweaks

--- player.cpp
+++ player.cpp
@@ -2,45 +2,53 @@
 #include <iostream>
 #include <cstdlib>
 
+//initializes player with 1000 cash
 Player::Player() {
 	cash = 1000;
 	bet = 0;
 	handValue = 0;
 }
 
+//initializes player with a specified amount of cash
 Player::Player(int money) {
 	this->cash = money;
 	bet = 0;
 	handValue = 0;
 }
 
+//sets the bet to be used for this hand
 void Player::SetBet(int playerBet) {
 	bet = playerBet;
 }
 
+//sets available cash for the player
 void Player::SetCash(int playerCash) {
 	cash = playerCash;
 }
 
+//returns the bet for this hand
 int Player::GetBet() const {
 	return bet;
 }
 
+//returns the available cash for the player
 int Player::GetCash() const {
 	return cash;
 }
 
+//returns the total value of the player's hand
 int Player::GetHandValue() const {
 	return handValue;
 }
 
+//pushes card into hand vector and adds card value to handValue
 void Player::DealCard(Card card) {
 	int randVal = rand() % 14 + 1;
 	int randSuit = rand() % 4 + 1;
 
-	char suit;
+	char suit{};
 
-	switch (randSuit) {
+	switch (randSuit) { //assigns a suit to the card
 		case 1:
 			suit = 'H';
 			break;
@@ -55,8 +63,8 @@ void Player::DealCard(Card card) {
 			break;
 	}
 
-	Card card(randVal, suit);
+	Card newCard(randVal, suit);
 
-	hand.push_back(card);
+	hand.push_back(newCard);
 	handValue += card.GetCardValue();
 }
